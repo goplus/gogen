3a9ee2e9f1184e9d278885d968c2d43d2b5d0270
visualfc <visualfc@gmail.com>
2021-09-03 13:15:30 +0800

    add TestAppend2

--- package_test.go
+++ package_test.go
@@ -1158,6 +1158,28 @@ func main() {
 `)
 }
 
+func TestAppend2(t *testing.T) {
+	pkg := newMainPackage()
+	builtin := pkg.Builtin()
+	tySlice := pkg.NewType("T").InitType(pkg, types.NewSlice(types.Typ[types.Int]))
+	pkg.NewFunc(nil, "foo", gox.NewTuple(pkg.NewParam(token.NoPos, "a", tySlice)), nil, false).BodyStart(pkg).
+		NewVar(tySlice, "b").VarRef(ctxRef(pkg, "b")).Val(builtin.Ref("append")).
+		Val(ctxRef(pkg, "b")).Val(ctxRef(pkg, "a")).Call(2, true).Assign(1).EndStmt().
+		End()
+	pkg.NewFunc(nil, "main", nil, nil, false).BodyStart(pkg).End()
+	domTest(t, pkg, `package main
+
+type T []int
+
+func foo(a T) {
+	var b T
+	b = append(b, a...)
+}
+func main() {
+}
+`)
+}
+
 func TestAppendString(t *testing.T) {
 	pkg := newMainPackage()
 	builtin := pkg.Builtin()
