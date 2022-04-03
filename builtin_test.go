d68bbad776e5c28760e0394c93f94d26ce18dc2f
xushiwei <x@goplus.org>
2022-04-03 22:31:53 +0800

    bfRefType.String

--- builtin_test.go
+++ builtin_test.go
@@ -383,8 +383,10 @@ func TestToVariadic(t *testing.T) {
 }
 
 func TestUnderlying(t *testing.T) {
+	bfReft := &bfRefType{}
 	typs := []types.Type{
 		&refType{},
+		bfReft,
 		&unboundType{},
 		&unboundMapElemType{},
 		&overloadFuncType{},
@@ -395,6 +397,9 @@ func TestUnderlying(t *testing.T) {
 		&unboundProxyParam{},
 		&TemplateParamType{},
 		&TemplateSignature{},
+	}
+	if bfReft.String() == "bfRefType{typ: nil:0 off: 0}" {
+		t.Fatal("bfRefType.String")
 	}
 	for _, typ := range typs {
 		func() {
