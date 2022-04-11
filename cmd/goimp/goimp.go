3555ee2fd54e43199d3fdf1d6c0efd4eb808bff3
xushiwei <x@goplus.org>
2022-04-11 08:31:59 +0800

    importer: use source compiler

--- cmd/goimp/goimp.go
+++ cmd/goimp/goimp.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"go/importer"
 	"go/token"
 	"go/types"
 	"os"
@@ -16,9 +17,14 @@ func main() {
 	os.Setenv("GOMODCACHE", val)
 	fmt.Println("GOMODCACHE:", val)
 
-	fset := token.NewFileSet()
-	packages := make(map[string]*types.Package)
-	imp := gcexportdata.NewImporter(fset, packages)
+	var fset = token.NewFileSet()
+	var imp types.Importer
+	if true {
+		imp = importer.ForCompiler(fset, "source", nil)
+	} else {
+		packages := make(map[string]*types.Package)
+		imp = gcexportdata.NewImporter(fset, packages)
+	}
 
 	_, err := imp.Import("go/types")
 	fmt.Println("Import result:", err)
