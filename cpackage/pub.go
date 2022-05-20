098e748b8658802e2d92dd28a610bc2b501f246d
xushiwei <x@goplus.org>
2022-05-20 11:02:23 +0800

    copyright

--- cpackage/pub.go
+++ cpackage/pub.go
@@ -11,25 +11,4 @@
  limitations under the License.
 */
 
-package main
-
-import (
-	"os"
-	"path/filepath"
-	"runtime"
-)
-
-func GOMODCACHE() string {
-	val := os.Getenv("GOMODCACHE")
-	if val == "" {
-		return filepath.Join(runtime.GOROOT(), "pkg/mod")
-	}
-	return val
-}
-
-func initGoEnv() {
-	val := os.Getenv("GOMODCACHE")
-	if val == "" {
-		os.Setenv("GOMODCACHE", filepath.Join(runtime.GOROOT(), "pkg/mod"))
-	}
-}
+package cpackage
