c40fc73a3ba2658a2271b7153e4517ac48f1403b
xushiwei <x@goplus.org>
2025-10-05 20:18:51 +0800

    matchType doc

--- ast.go
+++ ast.go
@@ -1236,7 +1236,7 @@ func (p *MatchError) Error() string {
 	return p.Message(pos.String() + ": ")
 }
 
-// TODO: use matchType to all assignable check
+// TODO(xsw): matchType vs. AssignableConv
 func matchType(pkg *Package, arg *internal.Elem, param types.Type, at interface{}) error {
 	if debugMatch {
 		cval := ""
