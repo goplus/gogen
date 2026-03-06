e73a95ea32cea6237e2635450165c0d59c4b1d18
visualfc <visualfc@gmail.com>
2026-03-06 11:03:11 +0800

    range check loadVar

--- stmt.go
+++ stmt.go
@@ -537,7 +537,9 @@ func (p *forRangeStmt) RangeAssignThen(cb *CodeBuilder, pos token.Pos) {
 			if name == "_" {
 				continue
 			}
-			if scope.Insert(types.NewVar(token.NoPos, pkg.Types, name, typs[i])) != nil {
+			newVar := types.NewVar(token.NoPos, pkg.Types, name, typs[i])
+			pkg.SetVarKind(newVar, LocalVar)
+			if scope.Insert(newVar) != nil {
 				log.Panicln("TODO: variable already defined -", name)
 			}
 		}
