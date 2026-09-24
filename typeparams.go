fbe1f0a7b3ebbb38ef3aef8bbcba4b0e8b2c4108
xushiwei <x@goplus.org>
2026-09-24 20:29:20 +0800

    toRecvType: support TypeParamList for alias type

--- typeparams.go
+++ typeparams.go
@@ -98,39 +98,37 @@ func newTypesContext() *typesContext {
 }
 
 func toRecvType(pkg *Package, typ types.Type) ast.Expr {
-	var t *types.Named
 	var tobj *types.TypeName
+	var tparams *types.TypeParamList
 	var star bool
 	if t, ok := typ.(*types.Pointer); ok {
 		typ = t.Elem()
 		star = true
 	}
-	t, ok := typ.(*types.Named)
-	if ok {
-		tobj = t.Obj()
-	} else if t2, ok2 := typ.(*types.Alias); ok2 {
-		tobj = t2.Obj()
-	} else {
+	switch t := typ.(type) {
+	case *types.Named:
+		tobj, tparams = t.Obj(), t.TypeParams()
+	case *types.Alias:
+		tobj, tparams = t.Obj(), t.TypeParams()
+	default:
 		panic("unexpected: recv type must types.Named or types.Alias")
 	}
 	expr := toObjectTypeExpr(pkg, tobj)
-	if t != nil {
-		if tparams := t.TypeParams(); tparams != nil {
-			n := tparams.Len()
-			indices := make([]ast.Expr, n)
-			for i := 0; i < n; i++ {
-				indices[i] = toObjectTypeExpr(pkg, tparams.At(i).Obj())
+	if tparams != nil {
+		n := tparams.Len()
+		indices := make([]ast.Expr, n)
+		for i := 0; i < n; i++ {
+			indices[i] = toObjectTypeExpr(pkg, tparams.At(i).Obj())
+		}
+		if n == 1 {
+			expr = &ast.IndexExpr{
+				X:     expr,
+				Index: indices[0],
 			}
-			if n == 1 {
-				expr = &ast.IndexExpr{
-					X:     expr,
-					Index: indices[0],
-				}
-			} else {
-				expr = &ast.IndexListExpr{
-					X:       expr,
-					Indices: indices,
-				}
+		} else {
+			expr = &ast.IndexListExpr{
+				X:       expr,
+				Indices: indices,
 			}
 		}
 	}
